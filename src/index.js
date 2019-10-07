76ff8ec45ed1c653b108749a60bc28a4481dd290
Den Moskvitin <denmoskvitin@gmail.com>
2019-10-07 22:10:48 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome product!`);
+console.log(`I would pay ${courseValue} for this awesome product!`); // eslint-disable-line no-console
