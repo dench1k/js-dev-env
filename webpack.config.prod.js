6648dbfbcf2b14bc2900acae77982b0429b98b15
Den Moskvitin <denmoskvitin@gmail.com>
2019-12-17 22:28:08 +0200

    add css cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import WebpackMd5Hash from "webpack-md5-hash";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
 
 export default {
   debug: true,
@@ -18,13 +19,18 @@ export default {
     filename: "[name].[chunkhash].js"
   },
   plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin("[name].[contenthash].css"),
+
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
+
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor"
     }),
+
     // Create HTML file that includes reference to bundled JS
     new HtmlWebpackPlugin({
       template: "src/index.html",
@@ -42,15 +48,17 @@ export default {
         minifyURLs: true
       }
     }),
+
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
+
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
-      { test: /\.css$/, loaders: ["style", "css"] }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("css?sourceMap") }
     ]
   }
 };
